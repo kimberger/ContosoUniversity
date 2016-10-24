a3693a4a2fa195ec9e8029ef68522523ed9cfc3f
Joseph Kimberger <another1liner@gmail.com>
2016-10-24 20:29:02 +0100

    The StringLength Attribute https://docs.asp.net/en/latest/data/ef-mvc/complex-data-model.html#the-stringlength-attribute

--- src/ContosoUniversity/Migrations/20161024192330_MaxLengthOnNames.Designer.cs
+++ src/ContosoUniversity/Migrations/20161024192330_MaxLengthOnNames.Designer.cs
@@ -8,9 +8,10 @@ using ContosoUniversity.Data;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20161024192330_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
