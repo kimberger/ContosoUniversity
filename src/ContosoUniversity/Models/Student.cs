e4695974cffbf187b3f341e354f442f42d91b159
Joseph Kimberger <another1liner@gmail.com>
2016-10-24 20:32:18 +0100

    The ColumnName Attribute https://docs.asp.net/en/latest/data/ef-mvc/complex-data-model.html#the-column-attribute

--- src/ContosoUniversity/Models/Student.cs
+++ src/ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -11,6 +12,7 @@ namespace ContosoUniversity.Models
         [StringLength(50)]
         public string LastName { get; set; }
 
+        [Column("FirstName")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
 
