dd8d35d98de197d90e956cf37ab26e3451416ceb
Joseph Kimberger <another1liner@gmail.com>
2016-10-25 23:40:51 +0100

    Update database context https://docs.asp.net/en/latest/data/ef-mvc/complex-data-model.html#update-the-database-context

--- src/ContosoUniversity/Data/SchoolContext.cs
+++ src/ContosoUniversity/Data/SchoolContext.cs
@@ -12,5 +12,15 @@ namespace ContosoUniversity.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
